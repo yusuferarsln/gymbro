834238b103aca1bfaa27afa8147d0260c780508b
yusuferarslangm <109597734+yusuferarslangm@users.noreply.github.com>
2022-10-21 16:06:02 +0300

    firebase conf

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
- 
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
