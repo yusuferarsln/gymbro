9e0fbbadefd45af11ff5e07bfe65a8640625283f
yusuferarslangm <109597734+yusuferarslangm@users.noreply.github.com>
2022-10-21 15:05:10 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+   await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+ 
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
